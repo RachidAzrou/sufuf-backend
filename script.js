7603dc2d32b0dddbf4ff7586b70fd35b29e2ea79
RachidAzrou <razrou@outlook.be>
2024-09-24 15:25:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function chooseRole(role) {
 // Functie voor de vrijwilliger om de status naar de server te sturen
 function sendStatus(status) {
     // Pas de URL aan naar je Render URL
-    fetch('https://<your-render-app-url>/status', {  // Vervang <your-render-app-url> met de werkelijke URL
+    fetch('https://sufuf-backend-2.onrender.com/status', {  // Vervang <your-render-app-url> met de werkelijke URL
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
